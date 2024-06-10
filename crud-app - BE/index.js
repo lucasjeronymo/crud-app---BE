d49a5e74e76548bd9b31d9d307fca5a527a43fc7
junhit-coder <thiagojunhonma@gmail.com>
2024-06-09 23:52:07 -0300

    Update index.js

--- crud-app - BE/index.js
+++ crud-app - BE/index.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/devweb';
-const SECRET_KEY = process.env.SECRET_KEY || 'your-secret-key';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://thiagojunhonma:AxToJqAgeQ0KcuDN@devweb.hmjkwxe.mongodb.net/?retryWrites=true&w=majority&appName=DevWeb';
+const SECRET_KEY = process.env.SECRET_KEY || 'chave-secreta';
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB conectado'))
